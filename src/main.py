6c2fe22f9e3f02eb53d7ebabdb70728aa309e7e9
CokeDividendos <coke.g.castro@gmail.com>
2025-07-08 18:48:10 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,1 @@
-import streamlit as st
-from auth import login_required, logout_button
 
-def main():
-    st.set_page_config(layout="wide")
-    if login_required():
-        st.sidebar.write("Usuario en sesión:", st.session_state.get("user"))
-        st.sidebar.write("Registro DB:", st.session_state.get("user_db"))
-        logout_button()
-        st.title("Demo Multiusuario")
-        st.write(f"Bienvenido, {st.session_state['user_db'][2]} ({st.session_state['user_db'][1]})")
-
-if __name__ == "__main__":
-    main()

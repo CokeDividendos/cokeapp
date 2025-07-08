a3d04197ee262e4b3ef4219f1619f5093230becb
CokeDividendos <coke.g.castro@gmail.com>
2025-07-08 09:08:42 -0400

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,7 @@
 # src/ui.py
 import streamlit as st
+import yfinance as yf
+from .services.yf_client import safe_history, history_resiliente, get_logo_url
 from .auth import get_nombre_usuario
 import textwrap
 
